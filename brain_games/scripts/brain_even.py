e11667ab530487277dd786058b7651d1b5a54eca
BaurNN <studentsoftpu@gmail.com>
2022-03-27 12:25:46 +0300

    fifth step of project

---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,38 @@
+from random import randint
+import prompt
+
+
+def main():
+    print('Welcome to the Brain Games!')
+    name = prompt.string('May I have your name? ')
+    print('Hello,', name + '!')
+    print('Answer "yes" if the number is even, otherwise answer "no"')
+    count = 0
+    while True:
+        number = randint(0, 101)
+        print(f'Question: {number}')
+        answer = input()
+        print(f'Your answer: {answer}')
+        if number % 2 == 0:
+            if answer == 'yes':
+                count += 1
+                print('Correct!')
+            else:
+                print(f"'{answer}' is wrong answer ;(. Correct answer was 'yes'")
+                print(f"Let's try again, {name}")
+                break
+        elif number % 2 != 0:
+            if answer == 'no':
+                count += 1
+                print('Correct!')
+            else:
+                print(f"'{answer}' is wrong answer ;(. Correct answer was 'no'")
+                print(f"Let's try again, {name}")
+                break
+        if count == 3:
+            print(f'Congratulations, {name}!')
+            break
+
+
+if __name__ == '__main__':
+    main()
